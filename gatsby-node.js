ce4c7efbea1dcfa7d220e0d85fc490b8b5be695e
Randy J. Ray <rjray@blackperl.com>
2019-07-30 22:20:35 -0700

    Add routing for origin, period, standard, mfr.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,9 @@ exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
-  let matched = page.path.match(/^\/(paint|mix)/)
+  let matched = page.path.match(
+    /^\/(paint|mix|origin|period|standard|manufacturer)\//
+  )
   if (matched) {
     page.matchPath = `/${matched[1]}/*`
 
