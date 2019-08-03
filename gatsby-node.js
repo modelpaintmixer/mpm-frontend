9b0a691db46242de956f1f4aedafd39c222de47b
Randy J. Ray <rjray@blackperl.com>
2019-08-02 21:58:49 -0700

    Rename mix to color in dyn paths.

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,7 @@ exports.onCreatePage = async ({ page, actions }) => {
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
   let matched = page.path.match(
-    /^\/(paint|mix|origin|period|standard|attribute|manufacturer)\//
+    /^\/(paint|color|origin|period|standard|attribute|manufacturer)\//
   )
   if (matched) {
     page.matchPath = `/${matched[1]}/*`
