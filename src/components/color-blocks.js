6491a1f47be92930cbdaa41db24f0e06884ee319
Randy J. Ray <rjray@blackperl.com>
2019-08-04 09:36:01 -0700

    Colors main page

---
+++ src/components/color-blocks.js
@@ -0,0 +1,46 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import ColorSwatch from "./color-swatch"
+import styles from "./color-blocks.module.css"
+
+const ColorBlock = ({ color }) => (
+  <div className={styles.colorBlock}>
+    <a
+      href={`/color/${color.id}`}
+      className={styles.colorLink}
+      title="View this color"
+    >
+      <div className={styles.colorSwatch}>
+        <ColorSwatch
+          color={color.colorRgb}
+          isTransparent={color.transparent}
+          isClear={color.clear}
+          size="4rem"
+          textSize="50%"
+        />
+      </div>
+      <p className={styles.colorTitle}>{color.name}</p>
+      <p className={styles.colorCredit}>Credit: {color.credit}</p>
+    </a>
+  </div>
+)
+
+ColorBlock.propTypes = {
+  color: PropTypes.object.isRequired,
+  key: PropTypes.any,
+}
+
+const ColorBlocks = ({ colors }) => (
+  <div className={styles.colorBlocks}>
+    {colors.map((color, index) => (
+      <ColorBlock key={index} color={color} />
+    ))}
+  </div>
+)
+
+ColorBlocks.propTypes = {
+  colors: PropTypes.array.isRequired,
+}
+
+export default ColorBlocks
--- src/pages/colors.js
+++ src/pages/colors.js
@@ -1,1 +1,80 @@
-"use strict"
+import React, { Component } from "react"
+
+import Layout from "../components/layout"
+import ColorBlocks from "../components/color-blocks"
+
+class ColorsPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      colors: null,
+      count: 0,
+      timeStamp: 0,
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/color")
+      .then(res => res.json())
+      .then(
+        result => {
+          let colors = result.colors.map(color => {
+            delete color.Origin
+            return color
+          })
+          this.setState({
+            isLoaded: true,
+            colors: colors,
+            count: colors.length,
+            timeStamp: result.timestamp,
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          })
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, colors, count } = this.state
+    let content
+
+    if (error) {
+      content = (
+        <>
+          <h2>An Error Occurred</h2>
+          <div className="text-block">
+            <p>An error occurred trying to load all the colors:</p>
+            <p>{error.message}</p>
+          </div>
+        </>
+      )
+    } else if (!isLoaded) {
+      content = (
+        <>
+          <div className="text-block">
+            <p>Loading...</p>
+          </div>
+        </>
+      )
+    } else {
+      content = (
+        <Layout title="All Colors">
+          <div className="text-block">
+            <h3>{count} Colors in Database</h3>
+            <ColorBlocks colors={colors} />
+          </div>
+        </Layout>
+      )
+    }
+
+    return content
+  }
+}
+
+export default ColorsPage
