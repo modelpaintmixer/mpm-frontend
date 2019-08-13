0926a825b764880e9694445655f55266df127ab1
Randy J. Ray <rjray@blackperl.com>
2019-08-13 13:52:43 -0700

    Renamed remaining content pages to MDX.

--- src/components/md-layout.js
+++ src/components/md-layout.js
@@ -5,12 +5,17 @@ import { MDXProvider } from "@mdx-js/react"
 import Layout from "./layout"
 import SEO from "./seo"
 
-const MdLayout = props => (
-  <MDXProvider>
-    <SEO title={props.pageContext.frontmatter.title} />
-    <Layout>{props.children}</Layout>
-  </MDXProvider>
-)
+const MdLayout = props => {
+  let title = props.pageContext.frontmatter.title
+  let heading = props.pageContext.frontmatter.heading || title
+
+  return (
+    <MDXProvider>
+      <SEO title={title} />
+      <Layout title={heading}>{props.children}</Layout>
+    </MDXProvider>
+  )
+}
 
 MdLayout.propTypes = {
   children: PropTypes.node.isRequired,
