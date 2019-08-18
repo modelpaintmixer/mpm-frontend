4dc139ee54a61f53c13b522e1363bb1febdc21d4
Randy J. Ray <rjray@blackperl.com>
2019-08-17 17:14:11 -0700

    Fix spacing in columns

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -7,24 +7,36 @@ import styles from "./main-navigation.module.css"
 const MainNavigation = () => (
   <section className={styles.navigationContainer}>
     <div className={`${styles.navigationColumn} text-block`}>
-      <h3>By Color Name</h3>
-      <NavigationColumn type="color" />
       <div>
-        <Link to="/colors">all colors...</Link>
+        <h3>By Color Name</h3>
+        <NavigationColumn type="color" />
+      </div>
+      <div>
+        <div>
+          <Link to="/colors">all colors...</Link>
+        </div>
       </div>
     </div>
     <div className={`${styles.navigationColumn} text-block`}>
-      <h3>By Period</h3>
-      <NavigationColumn type="period" />
       <div>
-        <Link to="/periods">all periods...</Link>
+        <h3>By Period</h3>
+        <NavigationColumn type="period" />
+      </div>
+      <div>
+        <div>
+          <Link to="/periods">all periods...</Link>
+        </div>
       </div>
     </div>
     <div className={`${styles.navigationColumn} text-block`}>
-      <h3>By Origin</h3>
-      <NavigationColumn type="origin" />
       <div>
-        <Link to="/origins">all origins...</Link>
+        <h3>By Origin</h3>
+        <NavigationColumn type="origin" />
+      </div>
+      <div>
+        <div>
+          <Link to="/origins">all origins...</Link>
+        </div>
       </div>
     </div>
   </section>
