ce4aecd52391bbe24aef26f081996e5da41552c7
Randy J. Ray <rjray@blackperl.com>
2019-08-23 16:06:40 -0700

    Copied rendering tweak for external/internal links

--- src/components/md-layout.js
+++ src/components/md-layout.js
@@ -1,16 +1,29 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 
 import Layout from "./layout"
 import SEO from "./seo"
+
+const checkExternal = props => {
+  if (props.href.toLowerCase().startsWith("http")) {
+    return <a target="_blank" {...props} />
+  } else {
+    return <Link to={props.href} {...props} />
+  }
+}
+
+const renderers = {
+  a: checkExternal,
+}
 
 const MdLayout = props => {
   let title = props.pageContext.frontmatter.title
   let heading = props.pageContext.frontmatter.heading || title
 
   return (
-    <MDXProvider>
+    <MDXProvider components={renderers}>
       <SEO title={title} />
       <Layout title={heading}>{props.children}</Layout>
     </MDXProvider>
