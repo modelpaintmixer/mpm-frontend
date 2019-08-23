c2d06f84bf01b115e705a9faf633549c51507dff
Randy J. Ray <rjray@blackperl.com>
2019-08-23 14:51:47 -0700

    Tweak rendering of external vs. internal links

--- src/components/render-notes.js
+++ src/components/render-notes.js
@@ -1,10 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 import ReactMarkdown from "react-markdown"
+
+const checkExternal = props => {
+  if (props.href.toLowerCase().startsWith("http")) {
+    return <a target="_blank" {...props} />
+  } else {
+    return <Link to={props.href} {...props} />
+  }
+}
+
+const renderers = {
+  link: checkExternal,
+  linkReference: checkExternal,
+}
 
 const RenderNotes = props => (
   <ReactMarkdown
     source={props.children}
+    renderers={renderers}
     skipHtml
     disallowedTypes={["image", "imageReference", "heading"]}
   />
